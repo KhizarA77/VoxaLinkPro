c7dd4244a63409dfe29b10443b2e1b60ac67d551
Khizar Asad <khizarcs123@gmail.com>
2023-12-09 20:59:21 +0500

    logger fixes

--- server/source/logger.js
+++ server/source/logger.js
@@ -1,12 +1,12 @@
-import winston from 'winston';
-import { fileURLToPath } from 'url';
-import path from 'path';
-import fs from 'fs';
+import winston from "winston";
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const logsDir = path.join(__dirname, 'logs');
+const logsDir = path.join(__dirname, "logs");
 if (!fs.existsSync(logsDir)) {
   fs.mkdirSync(logsDir);
 }
@@ -18,17 +18,17 @@ const customLevels = {
   http: 3,
   verbose: 4,
   debug: 5,
-  silly: 6
+  silly: 6,
 };
 
 const colors = {
-  error: 'red',
-  warn: 'yellow',
-  info: 'green',
-  http: 'magenta',
-  verbose: 'cyan',
-  debug: 'blue',
-  silly: 'grey'
+  error: "red",
+  warn: "yellow",
+  info: "green",
+  http: "magenta",
+  verbose: "cyan",
+  debug: "blue",
+  silly: "grey",
 };
 
 winston.addColors(colors);
@@ -37,42 +37,44 @@ const logger = winston.createLogger({
   levels: customLevels,
   format: winston.format.combine(
     winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
+      format: "YYYY-MM-DD HH:mm:ss",
     }),
     winston.format.errors({ stack: true }),
     winston.format.splat(),
     winston.format.colorize(),
-    winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    winston.format.printf(
+      (info) => `${info.timestamp} ${info.level}: ${info.message}`
+    )
   ),
   transports: [
     new winston.transports.File({
-      filename: path.join(logsDir, 'error.log'),
-      level: 'error',
+      filename: path.join(logsDir, "error.log"),
+      level: "error",
       handleExceptions: true,
       maxsize: 5242880, // 5MB
-      maxFiles: 5
+      maxFiles: 5,
     }),
     new winston.transports.File({
-      filename: path.join(logsDir, 'combined.log'),
+      filename: path.join(logsDir, "combined.log"),
       handleExceptions: true,
       maxsize: 5242880,
-      maxFiles: 5
-    })
+      maxFiles: 5,
+    }),
   ],
-  exitOnError: false
+  exitOnError: false,
 });
 
-// if (process.env.NODE_ENV !== 'production') {
-//   logger.add(new winston.transports.Console({
-//     format: winston.format.simple()
-//   }));
+logger.add(
+  new winston.transports.Console({
+    format: winston.format.simple(),
+  })
+);
 
-  // Override console.log
-//   const originalConsoleLog = console.log;
-//   console.log = (...args) => {
-//     logger.info(...args); // Redirect console.log to Winston's logger.info
-//     originalConsoleLog(...args); // Output to console as well
-//   };
-// }
+// Override console.log
+const originalConsoleLog = console.log;
+console.log = (...args) => {
+  logger.info(...args); // Redirect console.log to Winston's logger.info
+  originalConsoleLog(...args); // Output to console as well
+};
 
 export default logger;
