2419f8cef35009bd4392ad86960e0a5ff10ecaee
Khizar Asad <khizarcs123@gmail.com>
2023-12-09 14:53:34 +0500

    Fixed database deployment issues

--- server/source/connection.cjs
+++ server/source/connection.cjs
@@ -1,17 +1,17 @@
 const { Pool } = require("pg");
 
-const dotenv = require("dotenv");
-dotenv.config();
+// const dotenv = require("dotenv");
+// dotenv.config();
 let pool;
+
+
 if (process.env.NODE_ENV === "production") {
   pool = new Pool({
-    user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   });
-} else {
+}
+else {
   pool = new Pool({
     user: "Khizar",
     host: "localhost",
